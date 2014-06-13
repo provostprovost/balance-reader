34310b12e993b4ac49c29f70f45ace264836642c
Brian Provost <brianmprovost@gmail.com>
2014-06-13 07:00:00 -0500

    Convert two digit numbers to array of strings.

--- js/balance_reader.js
+++ js/balance_reader.js
@@ -1,8 +1,31 @@
-var balanceNum = 0;
+// Grab form input
+
+var balanceText = 0;
 
 $('#balanceForm').on('submit', function(e) {
   e.preventDefault();
-  balanceNum = $('#balanceText').val();
-  console.log(balanceNum);
+  balanceText = $('#balanceText').val();
+  console.log(balanceText);
   $('#balanceText').val("");
+
+  console.log(twoDigits(balanceText));
 });
+
+var twoDigits = function(digits) {
+  var num = parseInt(digits);
+  var numberText = [  "zero", "one", "two", "three", "four", "five",
+                      "six", "seven", "eight", "nine", "ten", "eleven",
+                      "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
+                      "seventeen", "eighteen", "nineteen"];
+  var tensText = [  "twenty", "thirty", "fourty", "fifty", "sixty",
+                    "seventy", "eighty", "ninety"];
+  if (num < 20) {
+    return [numberText[num]];
+  }
+  else {
+    var firstDigit = parseInt(digits[0]);
+    var secondDigit = parseInt(digits[1]);
+
+    return [tensText[firstDigit - 2], numberText[secondDigit]];
+  }
+};
